0d9ef7c32ed1d2be10f3150f12c095f5cb02842d
rigobertopo <rperez@cobach.edu.mx>
2025-03-31 16:59:28 -0600

    conexion a la BD

---
+++ Unach.Gastosdiarios.Conexion/Conexion.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unach.Gastosdiarios.Conexion
+{
+    public class Conexion
+    {
+        private readonly string cadenaConexion =
+           "server=localhost;port=3306;" +
+            "database=controlgastos;User Id=root";
+        public MySqlConnection ObtenerConexion()
+        {
+            MySqlConnection conectar =
+                new MySqlConnection();
+            try
+            {
+                conectar.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error");
+            }
+            return conectar;
+        }
+    }
+}
---
+++ Unach.Gastosdiarios.Conexion/GastosContext.cs
@@ -0,0 +1,40 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unach.Gastosdiarios.Conexion
+{
+    public class GastosContext
+    {
+        public List<Gasto> ObtenerGastos()
+        {
+            List<Gasto> _gastos =
+                        new List<Gasto>();
+            Conexion _conexion =
+                new Conexion();
+            using (MySqlConnection conn=
+                _conexion.ObtenerConexion())
+            {
+                string query = "Select * from Gastos";
+                MySqlCommand cmd = 
+                    new MySqlCommand(query, conn);
+                MySqlDataReader reader = 
+                    cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    _gastos.Add(new Gasto {Id=int.Parse(reader["Id"].ToString()),
+                        Descripcion=reader["Descripcion"].ToString(),
+                        Monto=decimal.Parse(reader["Monto"].ToString())
+
+
+                    });
+                }
+                return _gastos;
+            }
+        }
+        
+    }
+}
